189f48e0c6c0bc241484b3a85d9867732d128a91
Aman Sharma <amansharma.pcmip@outlook.com>
2019-03-21 01:26:22 +0530

    Always try to catch exceptions at high level of abstraction (note catch them with const reference to avoid accidental modifications). YOu should try not to catch them in low level of abstraction Like function itself. If needed in such cases you can catch error by returning a certain value.

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,33 @@
 
 int main()
 {
-  std::cout<<"scheck v1.0"<<std::endl;
-  Dictionary d("data/mydict.dat");
-  std::string word="dog";
-  while(getline(std::cin,word))
+  try
   {
-    if(d.check(word))
+    std::cout<<"scheck v1.0"<<std::endl;
+    Dictionary d("data/mydict.dat");
+    std::string word="dog";
+    while(getline(std::cin,word))
     {
-      std::cout<<word<<" is ok!"<<std::endl;
-    }
-    else
-    {
-      std::cout<<word<<" is Not present in Dictionary!"<<std::endl;
-    }
+      if(d.check(word))
+      {
+        std::cout<<word<<" is ok!"<<std::endl;
+      }
+      else
+      {
+        std::cout<<word<<" is Not present in Dictionary!"<<std::endl;
+      }
 
+    }
+  }
+  catch (const ScheckError &e )
+  {
+    std::cerr<<"Error "<<e.what()<<std::endl;
+    return 1;
+  }
+  catch()
+  {
+    std::cerr<<"Error: unknown exception"<<std::endl;
+    return 2;
   }
 
 }

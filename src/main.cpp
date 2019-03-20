21419a8f819aca671314b167daa57425ce08bb4a
Aman Sharma <amansharma.pcmip@outlook.com>
2019-03-21 00:25:48 +0530

    getting input form cin + Using dictionary

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,19 @@
 int main()
 {
   std::cout<<"scheck v1.0"<<std::endl;
-  Dictionary d("mydict.dat");
+  Dictionary d("data/mydict.dat");
   std::string word="dog";
-  if(d.check(word))
+  while(getline(std::cin,word))
   {
-    std::cout<<word<<" is ok!"<<std::endl;
+    if(d.check(word))
+    {
+      std::cout<<word<<" is ok!"<<std::endl;
+    }
+    else
+    {
+      std::cout<<word<<" is Not present in Dictionary!"<<std::endl;
+    }
+
   }
-  else
-  {
-    std::cout<<word<<" is Not present in Dictionary!"<<std::endl;
-  }
+
 }

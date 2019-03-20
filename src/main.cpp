43d5ebb3bd4d1a4093eff5f4a78e5955a1e7b7b3
Aman Sharma <amansharma.pcmip@outlook.com>
2019-03-20 22:34:52 +0530

    added dictionary support

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
+#include<string>
+#include "dictionary.h"
 
 int main()
 {
   std::cout<<"scheck v1.0"<<std::endl;
+  Dictionary d("mydict.dat");
+  std::string word="dog";
+  if(d.check(word))
+  {
+    std::cout<<word<<" is ok!"<<std::endl;
+  }
+  else
+  {
+    std::cout<<word<<" is Not present in Dictionary!"<<std::endl;
+  }
 }

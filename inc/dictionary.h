6469364e5052c79c6896e639f90e87cda4ae3af3
Aman Sharma <amansharma.pcmip@outlook.com>
2019-03-20 23:28:56 +0530

    added set to store dictionary words

--- inc/dictionary.h
+++ inc/dictionary.h
@@ -1,14 +1,17 @@
 #include<string>
+#include<set>
 
 class Dictionary
 {
+private:
+  std::set<std::string> mWords;
 public:
   Dictionary(const std::string &fname)
   {
-
+      mWords.insert("dog");
   };
   bool check(const std::string &word) const
   {
-    
+    return mWords.find(word)!=mWords.end();
   };
 };

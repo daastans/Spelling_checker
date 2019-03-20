e703dc09aee548322c5a0da04e12b33fc301216f
Aman Sharma <amansharma.pcmip@outlook.com>
2019-03-21 00:21:17 +0530

    implemented adding words to set by file + error handling using runtime_error class

--- inc/dictionary.h
+++ inc/dictionary.h
@@ -1,5 +1,7 @@
 #include<string>
 #include<set>
+#include <fstream>
+#include"error.h"
 
 class Dictionary
 {
@@ -8,7 +10,25 @@ private:
 public:
   Dictionary(const std::string &fname)
   {
-      mWords.insert("dog");
+    std::string word;
+    std::ifstream wlist(fname);
+    if(!wlist.is_open())
+    {
+      throw ScheckError(" could not open dictionary file" + fname);
+    }
+    else
+    {
+      std::cout<<fname<<" Dictionary Loaded"<<std::endl;
+    }
+    while(getline(wlist,word))
+    {
+      mWords.insert(word);
+    }
+    if ( ! wlist.eof() )
+    {
+       throw ScheckError( "Error reading dictionary file " + fname );
+    }
+
   };
   bool check(const std::string &word) const
   {

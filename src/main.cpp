10976d1b752fd46a6f970c34b08bcc94f1a6d3ac
Aman Sharma <amansharma.pcmip@gmail.com>
2019-04-06 14:07:38 +0530

    added CSV formatted error reporting

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "dictionary.h"
 #include "parser.h"
 #include "error.h"
+#include "csvreporter.h"
 
 int main()
 {
@@ -15,21 +16,25 @@ int main()
       throw ScheckError("could not open data/test.txt");
     }
     Parser p(sub);
+    CSVReporter rep(std::cout);
 
     Dictionary d("data/mydict.dat");
+    rep.ReportHeader();
 
     std::string word="dog";
     while ((word=p.NextWord())!="") {
       if(d.check(word))
       {
-        std::cout<<word<<" is ok!"<<std::endl;
+        //std::cout<<word<<" is ok!"<<std::endl;
       }
       else
       {
-        std::cout<<word<<" is Not present in Dictionary! at "<<p.LineNo()<<std::endl;
+        //std::cout<<word<<" is Not present in Dictionary! at "<<p.LineNo()<<std::endl;
+        rep.ReporterError(word,p.Context(),p.LineNo(),"data/test.txt");
       }
 
     }
+    rep.ReportFooter();
   }
   catch (const ScheckError &e )
   {

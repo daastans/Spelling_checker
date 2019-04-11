7c101fd5ee593b120a5d873f5bf3b9fce8775bc8
Aman Sharma <amansharma.pcmip@gmail.com>
2019-04-11 10:03:46 +0530

    Settings class implementation

---
+++ src/settings.cpp
@@ -0,0 +1,23 @@
+#include "settings.h"
+#include "commandline.h"
+
+
+const char* const DEF_DICT = "data/mydict.dat";
+const char* const DICT_OPT = "-d";
+const char* const CSV_OPT = "-csv";
+const char* const XML_OPT = "-xml";
+
+Settings::Settings(CommandLine& cmdl):mReportType(inCSV),mDictName(DEF_DICT)
+{
+  if(cmdl.HasOption(CSV_OPT)&&cmdl.HasOption(XML_OPT))
+    throw ScheckError("Only one export format can be specified\n");
+  if(cmdl.ExtractOptions(CSV_OPT))
+    mReportType=inCSV;
+  if(cmdl.ExtractOptions(XML_OPT))
+    mReportType=inXML;
+  cmdl.ExtractOptions(DICT_OPT,mDictName);
+  if(cmdl.MoreOptions())
+    throw ScheckError("Invalid command line\n");
+}
+Settings::Report Settings::ReportType() const {return mReportType;}
+std::string Settings::DictName() const {return mDictName;}

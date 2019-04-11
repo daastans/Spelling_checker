c903430a0e7e61255e1a2b8f8d37ca7ca0d54818
Aman Sharma <amansharma.pcmip@gmail.com>
2019-04-11 10:06:22 +0530

    created functions for if else(reporter obj creation) and while (word checking)

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,35 @@
 #include "csvreporter.h"
 #include "xmlreporter.h"
 #include "commandline.h"
+#include "settings.h"
 
+Reporter* MakeReporter(Settings::Report rt)
+{
+  if(rt==Settings::inCSV)
+    return new CSVReporter(std::cout);
+  else if(rt==Settings::inXML)
+    return new XMLReporter(std::cout);
+  else
+    throw ScheckError("Invalid Report Type\n");
+}
+void CheckSubmission(const Dictionary& dict,std::ifstream& sub,const std::string & subname ,Reporter& rep)
+{
+  Parser p(sub);
+  std::string word;
+  rep.ReportHeader();
+  while((word=p.NextWord())!="")
+  {
+    if(!dict.check(word))
+      rep.ReporterError(word,p.Context(),p.LineNo(),subname);
+  }
+  rep.ReportFooter();
+}
 int main(int argc,char *argv[])
 {
   try
   {
     std::string dict;
+    std::string filename="data/text.txt";
     std::cout<<"scheck v1.0"<<std::endl;
     std::ifstream sub("data/test.txt");
     if(!sub.is_open())
@@ -20,43 +43,15 @@ int main(int argc,char *argv[])
       throw ScheckError("could not open data/test.txt");
     }
     Parser p(sub);
-    //Reporter* rep=0;
     CommandLine cmdl(argc,argv);
+    Settings s(cmdl);
 
-    if(!cmdl.ExtractOptions("-d",dict));
-    {
-      dict="data/mydict.dat";
-    }
-    
-    std::unique_ptr <Reporter>rep;
-    if(cmdl.ExtractOptions("-xml"))
-    {
-      rep=std::unique_ptr<Reporter>(new XMLReporter(std::cout));
-    }
+    std::unique_ptr <Reporter>rep(MakeReporter(s.ReportType()));
+    Dictionary d(s.DictName());
+    CheckSubmission(d,sub,filename,*rep);
 
-    else
-    {
-      rep=std::unique_ptr<Reporter>(new CSVReporter(std::cout));
-    }
 
-    Dictionary d(dict);
-    rep->ReportHeader();
 
-    std::string word="dog";
-    while ((word=p.NextWord())!="") {
-      if(d.check(word))
-      {
-        //std::cout<<word<<" is ok!"<<std::endl;
-      }
-      else
-      {
-        //std::cout<<word<<" is Not present in Dictionary! at "<<p.LineNo()<<std::endl;
-        rep->ReporterError(word,p.Context(),p.LineNo(),"data/test.txt");
-      }
-
-    }
-    rep->ReportFooter();
-    //delete rep;//since it is dynamically created its memory management is in our hands
   }
   catch (const ScheckError &e )
   {
